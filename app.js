7ed91c9fb32ecabd6de4978e6968fafabfd0caab
Zhanxi Ni <ni.zhanxi@gmail.com>
2018-09-04 14:00:01 -0700

    Added MongDB and Review model, omg this was so hard

--- app.js
+++ app.js
@@ -2,18 +2,31 @@ var exphbs = require('express-handlebars');
 
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true});
+
+const Review = mongoose.model('Review', {
+    title: String
+});
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-let reviews = [
-    { title: "Great Review"},
-    { title: "Next Review"},
-    { title: "The Little Mermaid"}
-]
+// let reviews = [
+//     { title: "Great Review"},
+//     { title: "Next Review"},
+//     { title: "The Little Mermaid"}
+// ]
 
+//INDEX
 app.get('/', (req, res) => {
-    res.render('reviews-index', {reviews: reviews});
+    Review.find()
+        .then(reviews => {
+            res.render('reviews-index', {reviews: reviews});
+        })
+        .catch(err => {
+            console.log(err);
+        })
 })
 
 app.listen(3000, () => {
